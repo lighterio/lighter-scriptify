7fdd30901185a3b3fc0cfc21a41e9a3fe6426990
Sam Eubank <sameubank+zerious@gmail.com>
2015-09-17 08:24:14 -0700

    Initial commit

---
+++ test/scriptify-bench.js
@@ -0,0 +1,16 @@
+var fs = require('fs')
+var scriptify = require(__dirname + '/lighter-scriptify')
+
+describe('Benchmarks', function () {
+
+  bench('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+})
---
+++ test/scriptify-test.js
@@ -0,0 +1,26 @@
+var scriptify = require('../lighter-scriptify')
+var is = global.is || require('exam/lib/is')
+
+describe('scriptify', function () {
+
+  describe('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+  describe('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+})
